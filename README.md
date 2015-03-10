d1248a2ecfd47887270e4644d8be723fabaa0842
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-09 19:34:27 -0700

    functional car trip evaluator app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# prototype
+Car trip app given Trip Distance, Mile Per Gallon, Full Tank Gallons and Price Per Gallon, we can get:
+- The Total amount of stops to make to get to the destination.
+- The total Cost of the trip in dollars.
+- The amount of gallons left once we reach the destination.
--- client/main.js
+++ client/main.js
@@ -14,13 +14,13 @@ var ppg = $('#pricePerGal').val();
 var oneStopGal = ftg * ppg;
 var totalGal = dist / mpg;
 var totalStop = Math.ceil(totalGal / ftg);
-var totalCost = (totalGal * ppg);
+var totalCost = (totalGal * ppg).toFixed(2);
 var fullStopCost = ((Math.ceil(totalStop)) * oneStopGal);
-var galLeft = (fullStopCost - totalCost)/ ppg;
+var galLeft = ((fullStopCost - totalCost)/ ppg).toFixed(2);
 
-$('#totalStops').html('Total Stop is : ' +totalStop);
-$('#totalCost').html('Total Cost is : ' +totalCost);
-$('#galLeft').html('Gallons left is : ' +galLeft);
+$('#totalStops').html('Total Stop is : ' +totalStop+ ' stops');
+$('#totalCost').html('Total Cost of trip is : $' +totalCost);
+$('#galLeft').html('Gallons left upon arrival is : ' +galLeft+ ' gallons');
 
 // console log('total Stops: ' + totalStop+);
 // console log('total cost: ' + totalCost+);
